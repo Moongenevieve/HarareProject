977043c264f752b30c2ebdc081a58f71b77b47cb
Moongenevieve <fsamson5@gmail.com>
2025-09-13 14:04:53 +0100

    cors 2nd error fixed

--- src/main/java/com/example/HarareProject/config/SecurityConfig.java
+++ src/main/java/com/example/HarareProject/config/SecurityConfig.java
@@ -36,8 +36,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(withDefaults())
                 //csrf(csrf -> csrf.disable())
-
                 .authorizeHttpRequests(
                         auth ->
                                 auth.requestMatchers("/authenticate").permitAll()
