d3c2a0527d016b0714cfd44ade92c5b59436dc85
Moongenevieve <fsamson5@gmail.com>
2025-09-13 17:21:12 +0100

    changed swagger

--- src/main/java/com/example/HarareProject/config/SwaggerConfig.java
+++ src/main/java/com/example/HarareProject/config/SwaggerConfig.java
@@ -8,18 +8,29 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.List;
+
 
 @Configuration
 public class SwaggerConfig {
 
+
     @Bean
     public OpenAPI customOpenAPI() {
-
         return new OpenAPI()
                 .info(new Info().title("Harareproject"))
                 .addSecurityItem(new SecurityRequirement().addList("Harareproject"))
-                .components(new Components().addSecuritySchemes("Harareproject", new SecurityScheme()
-                        .name("Harareproject").type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
-
+                .components(new Components().addSecuritySchemes("Harareproject",
+                        new SecurityScheme()
+                                .name("Harareproject")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                // 👇 Add this block
+                .servers(List.of(
+                        new Server().url("https://harareproject-production.up.railway.app")
+                ));
     }
+
 }
